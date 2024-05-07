c9b22419e3ff2aff056ba8bd408b38ed762da0af
ashpatni20 <ash.patni20@gmail.com>
2024-05-07 11:02:49 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Quotes from './components/Quotes/Quotes.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Quotes></Quotes>
+
+  </React.StrictMode>,
+)
